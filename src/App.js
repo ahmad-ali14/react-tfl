d17195116dfe8e7daf8be2217a5ca9d0319df652
Ahmad Ali <aallii300300@gmail.com>
2020-06-06 02:37:18 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  var [modes, setModes] = useState([]);
+  var [initData, setInitData] = useState([]);
+  var [data, setData] = useState({});
+  var [singleMode, setSingleMode] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.tfl.gov.uk/Line/Route?ids=100&serviceTypes=Regular')
+      .then(res => setInitData(initData = res.data))
+  }, [])
+
+  useEffect(() => {
+    axios.get('https://api.tfl.gov.uk/Line/Meta/Modes')
+      .then(res => { setModes(modes = res.data); })
+  }, [])
+
+  useEffect(() => {
+    let temp = {};
+    if (initData.length > 0 && modes.length > 0) {
+      modes.map((t) => {
+        let fliteredData = initData.filter((d) => { return d.modeName == t.modeName });
+        temp[t.modeName] = fliteredData;
+
+      })
+    }
+    return setData(data = temp);
+  }, [modes, initData])
+
+  const selectMode = (v) => {
+    setSingleMode(singleMode = v);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Transport For London React Hooks </h1>
+      {<ModesDropDown
+        types={modes}
+        selectMode={selectMode}
+      />}
     </div>
   );
 }
 
 export default App;
+
+
+const ModesDropDown = ({ types, selectMode }) => {
+  return (
+    <select onChange={e => { selectMode(e.target.value) }}>
+      {types.length > 1 && types.map((t) => {
+        return (
+          <option key={t.modeName} value={t.modeName} > {t.modeName}</option>
+        );
+      })}
+    </select>
+  )
+}
